cd9ed3ec8186c1e676440a7914f75c493ce4014d
Tobias Kräntzer <tobias@nxtbgthng.com>
2013-12-16 16:27:58 +0100

    Initial Commit

---
+++ CorePitch/CorePitch/CorePitch.h
@@ -0,0 +1,13 @@
+//
+//  CorePitch.h
+//  CorePitch
+//
+//  Created by Tobias Kräntzer on 16.12.13.
+//  Copyright (c) 2013 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CorePitch : NSObject
+
+@end
---
+++ CorePitch/CorePitch/CorePitch.m
@@ -0,0 +1,13 @@
+//
+//  CorePitch.m
+//  CorePitch
+//
+//  Created by Tobias Kräntzer on 16.12.13.
+//  Copyright (c) 2013 Tobias Kräntzer. All rights reserved.
+//
+
+#import "CorePitch.h"
+
+@implementation CorePitch
+
+@end
